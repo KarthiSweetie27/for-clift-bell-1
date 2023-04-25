44afc0ecf6333b8aa8cae73a3641b206cdff2989
KarthiSweetie27 <122539938+KarthiSweetie27@users.noreply.github.com>
2023-04-25 08:57:17 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+function arm_up () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 20)
+}
+function forward () {
+    wuKong.setAllMotor(20, 20)
+}
+function arm_down () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+}
+function Backward () {
+    wuKong.setAllMotor(-20, -20)
+}
+joystickbit.initJoystickBit()
+radio.setGroup(25)
+basic.showLeds(`
+    # . . # .
+    # . # . .
+    # # . . .
+    # . # . .
+    # . . # .
+    `)
+basic.forever(function () {
+	
+})
