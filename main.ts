4eb2039a8a5e9af898340ef05ace44a67eeadb01
KarthiSweetie27 <122539938+KarthiSweetie27@users.noreply.github.com>
2023-04-26 10:02:16 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 function arm_up () {
     wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 20)
 }
+function forward2 () {
+    wuKong.stopAllMotor()
+}
 function forward () {
     wuKong.setAllMotor(20, 20)
 }
